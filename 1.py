f708840ba772c9957ffe58eff97294b1d08d87f2
Aruvelut <2502474438@qq.com>
2023-01-06 21:14:23 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,26 @@
+import time
+import random
+import sys
+import tkinter
+from tkinter import messagebox
+
+a = input('你是歌姬吗？')
+b = '是'
+c = '否'
+d = '钝角'
+e = 'e'
+if a == b:
+    print('你是歌姬吧')
+    time.sleep(3)
+if a == c:
+    print('NND，不是也别想活着！')
+    time.sleep(3)
+if a == d:
+    messagebox.showinfo(title='test',message='test')
+    messagebox.showerror(title='test',message='test')
+    messagebox.showwarning(title='import hacktool hacktool.hack(127.0.0.1)',message='Hacker:一起玩Minecraft吧')
+if a == e:
+    print('你一定是kxkl才这么试吧awa')
+    messagebox.showinfo(title='屑',message='SANTALE是个屑')
+    messagebox.showerror(title='屑',message='lirizz是个屑')
+    messagebox.showwarning(title='屑',message='kxkl是个屑')
